787a6b2af9b17b5611f465dc654d80b60480d1e1
antfou <fougneranton@gmail.com>
2022-10-28 13:53:45 +0200

    added "addPieces" to logic class

--- src/GameFiles/Graphics.java
+++ src/GameFiles/Graphics.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Graphics extends JFrame {
-
+    Logic logic = new Logic();
     Random r = new Random();
     JPanel basePanel = new JPanel();
     JPanel northPanel = new JPanel();
@@ -15,31 +15,14 @@ public class Graphics extends JFrame {
     JButton newGame = new JButton("Nytt Spel");
     JButton[] numButtons= new JButton[16];
     JButton emptyButton = new JButton("Empty");
+
         Graphics(){
             basePanel.setLayout(new BorderLayout());
             southPanel.setLayout(new GridLayout(4,4));
             northPanel.add(newGame);
 
-            ArrayList<Integer> list = new ArrayList<>();
 
-            for (int i=1; i<=16; i++) { //Randomizes numbers from 1-15
-                    list.add(i);
-            }
-
-            Collections.shuffle(list);
-
-            for (int i = 0; i <=15; i++) {
-                if(list.get(i) != 16) {
-                    numButtons[i] = new JButton("" + list.get(i));
-                    southPanel.add(numButtons[i]);
-                }
-                else{
-                    emptyButton.setBackground(Color.WHITE);
-                    southPanel.add(emptyButton);
-                }
-
-            }
-
+            logic.addPieces(southPanel,numButtons,emptyButton);
             add(basePanel);
             basePanel.add(northPanel, BorderLayout.NORTH);
             basePanel.add(southPanel);
@@ -48,6 +31,9 @@ public class Graphics extends JFrame {
             setVisible(true);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
         }
+
+
+
 
 
 }
--- src/GameFiles/Logic.java
+++ src/GameFiles/Logic.java
@@ -1,4 +1,36 @@
 package GameFiles;
 
-public class Logic {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Logic{
+    JFrame jfr = new JFrame();
+
+    protected void addPieces(JPanel panel, JButton[] buttons, JButton emptyButton){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i=1; i<=16; i++) { //Randomizes numbers from 1-15
+            list.add(i);
+        }
+
+        Collections.shuffle(list);
+
+        for (int i = 0; i <=15; i++) {
+            if(list.get(i) != 16) {
+                buttons[i] = new JButton("" + list.get(i));
+                panel.add(buttons[i]);
+            }
+            else{
+                emptyButton.setBackground(Color.WHITE);
+                panel.add(emptyButton);
+            }
+
+        }
+
+    }
+
+
+
 }
