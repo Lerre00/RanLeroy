402ba80944e631a083c3c8cd6363dcaef4e586bf
Leroy Collazo <leroycollazo@hotmail.com>
2022-10-28 12:33:21 +0200

    fixed code for buttons

--- src/GameFiles/Graphics.java
+++ src/GameFiles/Graphics.java
@@ -2,6 +2,8 @@ package GameFiles;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Graphics extends JFrame {
@@ -11,27 +13,26 @@ public class Graphics extends JFrame {
     JPanel northPanel = new JPanel();
     JPanel southPanel = new JPanel();
     JButton newGame = new JButton("Nytt Spel");
-    JButton[][] button = new JButton[4][4];
+    JButton[] numButtons= new JButton[17];
     JButton emptyButton = new JButton("Empty");
-
         Graphics(){
             basePanel.setLayout(new BorderLayout());
             southPanel.setLayout(new GridLayout(4,4));
             northPanel.add(newGame);
 
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    if (i!=3 || j!=3) {
-                        button[i][j] = new JButton();
-                        button[i][j].setBackground(Color.getHSBColor(r.nextFloat(),r.nextFloat(),r.nextFloat()));
-                        southPanel.add(button[i][j]);
-                    }
-                    else {
+            //int test = r.nextInt(10-1 + 1) + 1;
+
+                for (int i = 1; i < 17; i++) {
+                    if (i != 16) {
+                        numButtons[i] = new JButton(""+i);
+                        //numButtons[i].setBackground(Color.getHSBColor(r.nextFloat(),r.nextFloat(),r.nextFloat()));
+                        southPanel.add(numButtons[i]);
+                    } else {
                         emptyButton.setBackground(Color.WHITE);
                         southPanel.add(emptyButton);
                     }
                 }
-            }
+
             add(basePanel);
             basePanel.add(northPanel, BorderLayout.NORTH);
             basePanel.add(southPanel);
