dbdc4427649948192647c87453157caead8d71d4
Leroy Collazo <leroycollazo@hotmail.com>
2022-10-28 13:25:28 +0200

    added randomizer

--- src/GameFiles/Graphics.java
+++ src/GameFiles/Graphics.java
@@ -13,25 +13,32 @@ public class Graphics extends JFrame {
     JPanel northPanel = new JPanel();
     JPanel southPanel = new JPanel();
     JButton newGame = new JButton("Nytt Spel");
-    JButton[] numButtons= new JButton[17];
+    JButton[] numButtons= new JButton[16];
     JButton emptyButton = new JButton("Empty");
         Graphics(){
             basePanel.setLayout(new BorderLayout());
             southPanel.setLayout(new GridLayout(4,4));
             northPanel.add(newGame);
 
-            //int test = r.nextInt(10-1 + 1) + 1;
+            ArrayList<Integer> list = new ArrayList<>();
 
-                for (int i = 1; i < 17; i++) {
-                    if (i != 16) {
-                        numButtons[i] = new JButton(""+i);
-                        //numButtons[i].setBackground(Color.getHSBColor(r.nextFloat(),r.nextFloat(),r.nextFloat()));
-                        southPanel.add(numButtons[i]);
-                    } else {
-                        emptyButton.setBackground(Color.WHITE);
-                        southPanel.add(emptyButton);
-                    }
+            for (int i=1; i<=16; i++) { //Randomizes numbers from 1-15
+                    list.add(i);
+            }
+
+            Collections.shuffle(list);
+
+            for (int i = 0; i <=15; i++) {
+                if(list.get(i) != 16) {
+                    numButtons[i] = new JButton("" + list.get(i));
+                    southPanel.add(numButtons[i]);
                 }
+                else{
+                    emptyButton.setBackground(Color.WHITE);
+                    southPanel.add(emptyButton);
+                }
+
+            }
 
             add(basePanel);
             basePanel.add(northPanel, BorderLayout.NORTH);
