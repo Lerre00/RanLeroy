e4b5a00becfff7080cd05d291313383f76ff05a9
Leroy Collazo <leroycollazo@hotmail.com>
2022-10-31 14:26:12 +0100

    fresh push

--- src/GameFiles/Logic.java
+++ src/GameFiles/Logic.java
@@ -2,10 +2,12 @@ package GameFiles;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Logic{
+public class Logic {
     JFrame jfr = new JFrame();
 
     protected void restartGame(JButton button, JPanel panel, JButton[] buttons, JButton emptyButton){
@@ -21,6 +23,7 @@ public class Logic{
     protected void addPieces(JPanel panel, JButton[] buttons, JButton emptyButton){
 
         ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<JButton> buttonArr = new ArrayList<>();
 
         for (int i=1; i<=16; i++) { //Randomizes numbers from 1-15
             list.add(i);
@@ -31,18 +34,17 @@ public class Logic{
         for (int i = 0; i <=15; i++) {
             if(list.get(i) != 16) {
                 buttons[i] = new JButton("" + list.get(i));
+                String name = buttons[i].getName();
                 panel.add(buttons[i]);
             }
             else{
                 emptyButton.setBackground(Color.WHITE);
                 panel.add(emptyButton);
             }
+            buttonArr.add(buttons[i]);
 
         }
 
-
     }
-
-
 
 }
