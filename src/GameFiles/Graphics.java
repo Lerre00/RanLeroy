a17174d45ed4e871c1944bb43d9e441b2704546e
Leroy Collazo <leroycollazo@hotmail.com>
2022-11-02 13:05:52 +0100

    finalized graphics

--- src/GameFiles/Graphics.java
+++ src/GameFiles/Graphics.java
@@ -2,52 +2,39 @@ package GameFiles;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
 
 public class Graphics extends JFrame {
 
-    Random r = new Random();
+    Logic logic = new Logic();
     JPanel basePanel = new JPanel();
     JPanel northPanel = new JPanel();
     JPanel southPanel = new JPanel();
-    JButton newGame = new JButton("Nytt Spel");
-    JButton[] numButtons= new JButton[16];
-    JButton emptyButton = new JButton("Empty");
-        Graphics(){
-            basePanel.setLayout(new BorderLayout());
-            southPanel.setLayout(new GridLayout(4,4));
-            northPanel.add(newGame);
+    JButton restartButton = new JButton("Nytt Spel");
+    JButton[] buttonArr = new JButton[16];
+    JLabel winLabel = new JLabel("You Won!");
 
-            ArrayList<Integer> list = new ArrayList<>();
 
-            for (int i=1; i<=16; i++) { //Randomizes numbers from 1-15
-                    list.add(i);
-            }
+    Graphics(){
+        basePanel.setLayout(new BorderLayout());
+        southPanel.setLayout(new GridLayout(4,4));
+        northPanel.add(restartButton);
+        winLabel.setFont(new Font("Arial", Font.BOLD, 32));
+        winLabel.setHorizontalAlignment(JLabel.CENTER);
 
-            Collections.shuffle(list);
+        logic.startGame(southPanel, buttonArr);
+        logic.winGame(buttonArr, southPanel, winLabel);
+        logic.movePieces(buttonArr);
+        logic.restartGame(restartButton, southPanel, buttonArr, winLabel);
+        add(basePanel);
 
-            for (int i = 0; i <=15; i++) {
-                if(list.get(i) != 16) {
-                    numButtons[i] = new JButton("" + list.get(i));
-                    southPanel.add(numButtons[i]);
-                }
-                else{
-                    emptyButton.setBackground(Color.WHITE);
-                    southPanel.add(emptyButton);
-                }
+        basePanel.add(northPanel, BorderLayout.NORTH);
+        basePanel.add(southPanel);
 
-            }
+        setSize(500,500);
+        setLocation(100,100);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-            add(basePanel);
-            basePanel.add(northPanel, BorderLayout.NORTH);
-            basePanel.add(southPanel);
-            setSize(500,500);
-            setLocation(100,100);
-            setVisible(true);
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
-        }
-
+    }
 
 }
